de8575552d7fbe9f8fa8b62cfdc506ec587d95e9
Enver Fransazov <enverfransazov@gmail.com>
2026-02-15 10:49:41 +0200

    adding more example

--- 08.NestedLoops/02.EvenPowersOf2/Program.cs
+++ 08.NestedLoops/02.EvenPowersOf2/Program.cs
@@ -1,1 +1,9 @@
 ﻿
+int n = int.Parse(Console.ReadLine());
+
+int number = 0;
+while (number <= n)
+{
+    Console.WriteLine(Math.Pow(2, number));
+    number += 2;
+}
--- 08.NestedLoops/03.TriangleOfStars/Program.cs
+++ 08.NestedLoops/03.TriangleOfStars/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+int sizeOfTriangle = int.Parse(Console.ReadLine());
+
+for (int row = 1; row <= sizeOfTriangle; row++)
+{
+	for (int col = 1; col <= row; col++)
+	{
+        Console.Write("*");
+	}
+	Console.WriteLine();
+}
+
