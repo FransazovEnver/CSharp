0ba34ac840c42c5dd05de1285849e22fe383e534
Enver Fransazov <enverfransazov@gmail.com>
2026-02-17 13:45:36 +0200

    add new exercises

---
+++ 08.NestedLoops/07.SumOfDigitsCalculator/07.SumOfDigitsCalculator.cs
@@ -0,0 +1,23 @@
+﻿
+string input = Console.ReadLine();
+
+while (input != "End")
+{
+    int number = int.Parse(input);
+
+    int sumDigits = 0;
+    while (number > 0)
+    {
+        int lastDigits = number % 10;
+
+        sumDigits += lastDigits;
+
+        number /= 10;
+    }
+
+    Console.WriteLine("Sum of digits = " + sumDigits);
+
+    input = Console.ReadLine();
+}
+
+Console.WriteLine("Goodbye");
