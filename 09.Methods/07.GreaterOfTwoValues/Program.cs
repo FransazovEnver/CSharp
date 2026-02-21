2a212e3b44b85ad0541cddcc1526bbedac95b38a
Enver Fransazov <enverfransazov@gmail.com>
2026-02-21 21:03:55 +0200

    Add solutions to sum ex.

--- 09.Methods/07.GreaterOfTwoValues/Program.cs
+++ 09.Methods/07.GreaterOfTwoValues/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string type = Console.ReadLine();
+
+PrintGraterValue(type);
+
+static void PrintGraterValue(string type)
+{
+    switch (type)
+    {
+        case "int":
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine(int.Max(firstNumber, secondNumber));
+            break;
+        case "string":
+            string firstText = Console.ReadLine();
+            string secondText = Console.ReadLine();
+            if (String.Compare(firstText, secondText) >= 0)
+            {
+                Console.WriteLine(firstText);
+            }
+            else
+            {
+                Console.WriteLine(secondText);
+            }
+            break;
+        case "char":
+            char firstSymbol = char.Parse(Console.ReadLine());
+            char secondSymbol = char.Parse(Console.ReadLine());
+            if (firstSymbol <= secondSymbol)
+            {
+                Console.WriteLine(secondSymbol);
+            }
+            else
+            {
+                Console.WriteLine(firstSymbol);
+            }
+            break;
+    }
+}
