ad3aae81903218e1236a92230f93162864df9afa
Enver Fransazov <enverfransazov@gmail.com>
2026-02-21 15:59:31 +0200

    new ex. adding

---
+++ 09.Methods/01.SignOfIntegerNumber/Program.cs
@@ -0,0 +1,21 @@
+﻿int number = int.Parse(Console.ReadLine());
+
+PrintSign(number);
+
+static int PrintSign(int number)
+{
+    if (number > 0)
+    {
+        Console.WriteLine($"The number {number} is positive.");
+    }
+    else if (number < 0)
+    {
+        Console.WriteLine($"The number {number} is negative.");
+    }
+    else if (number == 0)
+    {
+        Console.WriteLine($"The number {number} is zero.");
+    }
+
+    return number
+}
