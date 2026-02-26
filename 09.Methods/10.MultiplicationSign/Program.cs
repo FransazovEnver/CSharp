15c2ecfeff341f259f6a67e9369e4633bb39e94b
Enver Fransazov <enverfransazov@gmail.com>
2026-02-26 09:20:20 +0200

    Add some solution

--- 09.Methods/10.MultiplicationSign/Program.cs
+++ 09.Methods/10.MultiplicationSign/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+int num3 = int.Parse(Console.ReadLine());
+
+ResultSign(num1, num2, num3);
+
+static void ResultSign(int x, int y, int z)
+{
+    string result = "";
+    if (x == 0 || y == 0 || z == 0)
+    {
+        result = "zero";
+    }
+    else if ((x > 0 && y > 0 && z > 0)
+        || (x < 0 && y < 0 && z > 0)
+        || (x < 0 && y > 0 && z < 0)
+        || (x > 0 && y < 0 && z < 0))
+    {
+        result = "positive";
+    }
+    else 
+    {
+        result = "negative";
+    }
+    Console.WriteLine(result);
+}
+
