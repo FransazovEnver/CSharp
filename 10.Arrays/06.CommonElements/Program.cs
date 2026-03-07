fbceb80c54b5ec27fca66c308eb71fd0ba200ccd
Enver Fransazov <enverfransazov@gmail.com>
2026-03-07 12:27:09 +0200

    feat: solution to common elements with two for loops

--- 10.Arrays/06.CommonElements/Program.cs
+++ 10.Arrays/06.CommonElements/Program.cs
@@ -1,1 +1,21 @@
 ﻿
+int[] firstArr = Console.ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .ToArray();
+int[] secondArr = Console.ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .ToArray();
+
+
+for (int i = 0; i < firstArr.Length; i++)
+{
+    for (int j = 0; j < secondArr.Length; j++)
+    {
+        if (firstArr[i] == secondArr[j])
+        {
+            Console.Write($"{firstArr[i]}" + " ");
+        }
+    }
+}
