11fa8f9e0454de1c89955418150c340a754d34fb
Enver Fransazov <enverfransazov@gmail.com>
2026-03-07 05:21:43 +0200

    New feature adding

--- 10.Arrays/01.DayOfWeek/Program.cs
+++ 10.Arrays/01.DayOfWeek/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+int dayOfWeek = int.Parse(Console.ReadLine());
+
+string[] arraysDayOfweek = new string[7] {
+
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+};
+
+if (dayOfWeek >= 1 && dayOfWeek <= 7)
+{
+    Console.WriteLine(arraysDayOfweek[dayOfWeek - 1]);
+}
+else
+    Console.WriteLine("Invalid day!");
