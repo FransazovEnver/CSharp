f10b941719a41b4482579d9115a7cae492f4cf53
Enver Fransazov <enverfransazov@gmail.com>
2026-03-07 06:31:03 +0200

    reverse array solution

--- 10.Arrays/03.ReverseAnArray/Program.cs
+++ 10.Arrays/03.ReverseAnArray/Program.cs
@@ -1,1 +1,14 @@
-﻿);
+﻿
+int n = int.Parse(Console.ReadLine());
+
+int[] array = new int[n];
+
+for (int i = 0; i < n; i++)
+{
+    array[i] = int.Parse(Console.ReadLine());
+}
+
+for (int i = n - 1; i >= 0; i--)
+{
+    Console.Write(array[i] + " ");
+}
