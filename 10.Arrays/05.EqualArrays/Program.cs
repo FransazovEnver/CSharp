5b6fff147a1f9f9cf785b9ce9645d0e81c789d4e
Enver Fransazov <enverfransazov@gmail.com>
2026-03-07 09:09:26 +0200

    compare a two arrays

--- 10.Arrays/05.EqualArrays/Program.cs
+++ 10.Arrays/05.EqualArrays/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+int[] firstArr = Console.ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .ToArray();
+int[] secondArr = Console.ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .ToArray();
+
+
+bool areEqual = true;
+if (firstArr.Length == secondArr.Length)
+{
+    for (int i = 0; i < firstArr.Length; i++)
+    {
+        if (firstArr[i] != secondArr[i])
+        {
+            areEqual = false;
+            break;
+        }
+    }
+
+}
+else
+{
+    areEqual = false;
+    Console.WriteLine("Arrays are not identical.");
+}
+
+
