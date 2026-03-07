efd03775d8db80cd0b0c8b8fb854bdbf079a01f3
Enver Fransazov <enverfransazov@gmail.com>
2026-03-07 05:50:21 +0200

    solution to sumArrays with sum += number[i]

--- 10.Arrays/02.SumAnArray/Program.cs
+++ 10.Arrays/02.SumAnArray/Program.cs
@@ -1,1 +1,14 @@
 ﻿
+int[] number = Console.ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .ToArray();
+
+int sum = 0;
+
+for (int i = 0; i < number.Length; i++)
+{
+    sum += number[i];
+}
+
+Console.WriteLine(sum);
