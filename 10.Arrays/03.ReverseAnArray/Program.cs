749f688fdd168cfc4144a758d0829aadf3225ce1
Enver Fransazov <enverfransazov@gmail.com>
2026-03-07 08:08:04 +0200

    Some array solution

--- 10.Arrays/03.ReverseAnArray/Program.cs
+++ 10.Arrays/03.ReverseAnArray/Program.cs
@@ -3,6 +3,7 @@ int n = int.Parse(Console.ReadLine());
 
 int[] array = new int[n];
 
+
 for (int i = 0; i < n; i++)
 {
     array[i] = int.Parse(Console.ReadLine());
--- 10.Arrays/04.EvenAndOddSubtraction/Program.cs
+++ 10.Arrays/04.EvenAndOddSubtraction/Program.cs
@@ -1,1 +1,23 @@
 ﻿
+int[] arrNumber = Console.ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .ToArray();
+
+int odd = 0;
+int even = 0;
+
+foreach (int number in arrNumber)
+{
+
+    if (number % 2 == 0)
+    {
+        even += number;
+    }
+    else 
+    {
+        odd += number;
+    }
+}
+
+Console.WriteLine(even - odd);
