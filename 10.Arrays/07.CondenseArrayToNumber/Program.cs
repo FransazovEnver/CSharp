e039b704fba2a70efdcac1337f0df80372049c54
Enver Fransazov <enverfransazov@gmail.com>
2026-03-08 11:24:34 +0200

    solution of sumArray

--- 10.Arrays/07.CondenseArrayToNumber/Program.cs
+++ 10.Arrays/07.CondenseArrayToNumber/Program.cs
@@ -4,16 +4,22 @@ int[] mainArray = Console.ReadLine()
                       .Select(int.Parse)
                       .ToArray();
 
+
 while (mainArray.Length > 1)
 {
+    //second array who take "- 1 index" from main array
     int[] condenseArray = new int[mainArray.Length - 1];
 
+    //For loop who sum array indexes  
     for (int i = 0; i < mainArray.Length - 1; i++)
     {
+        //sum index by index 
         int currSum = mainArray[i] + mainArray[i + 1];
+
+        //second array take a sum value
         condenseArray[i] = currSum;
     }
-
+    //make a main array in small array to become one index and print
     mainArray = condenseArray;
 }
 
