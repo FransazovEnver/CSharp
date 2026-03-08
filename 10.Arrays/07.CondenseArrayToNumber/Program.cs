8ca1629927c631e1bc482d801834950a6a8d3ffc
Enver Fransazov <enverfransazov@gmail.com>
2026-03-08 11:23:58 +0200

    solution of sumArray

--- 10.Arrays/07.CondenseArrayToNumber/Program.cs
+++ 10.Arrays/07.CondenseArrayToNumber/Program.cs
@@ -1,1 +1,20 @@
 ﻿
+int[] mainArray = Console.ReadLine()
+                      .Split(" ")
+                      .Select(int.Parse)
+                      .ToArray();
+
+while (mainArray.Length > 1)
+{
+    int[] condenseArray = new int[mainArray.Length - 1];
+
+    for (int i = 0; i < mainArray.Length - 1; i++)
+    {
+        int currSum = mainArray[i] + mainArray[i + 1];
+        condenseArray[i] = currSum;
+    }
+
+    mainArray = condenseArray;
+}
+
+Console.WriteLine(mainArray[0]);
