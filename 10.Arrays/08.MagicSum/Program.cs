a42c729730e88183ac9c867c17ce88f75582b638
Enver Fransazov <enverfransazov@gmail.com>
2026-03-09 11:51:38 +0200

    adding some solutions

--- 10.Arrays/08.MagicSum/Program.cs
+++ 10.Arrays/08.MagicSum/Program.cs
@@ -1,1 +1,24 @@
 ﻿
+int[] numbresArray = Console.ReadLine()
+                      .Split(" ")
+                      .Select(int.Parse)
+                      .ToArray();
+
+
+int controlNumber = int.Parse(Console.ReadLine());
+
+
+for (int i = 0; i < numbresArray.Length - 1; i++)
+{
+    int currentElement = numbresArray[i];
+    for (int j = i + 1; j < numbresArray.Length ; j++)
+    {
+        int nextElement = numbresArray[j];
+        if (currentElement + nextElement == controlNumber)
+        {
+            Console.WriteLine(currentElement + " " + nextElement);
+            break;
+        }
+    }
+}
+                      
